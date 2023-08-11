3903e8e1d736f2d33620b53de9e7e4b6f1c864e1
unknown <kamleshchandel04@gmail.com>
2023-08-11 10:22:15 +0530

    sql

---
+++ ques 3.sql
@@ -0,0 +1,12 @@
+-- Show first name of patients that start with the letter 'C'
+
+
+select first_name
+from patients
+where first_name like "C%";
+
+
+
+-- SELECT first_name
+-- FROM patients
+-- WHERE substring(first_name, 1, 1) = 'C'
